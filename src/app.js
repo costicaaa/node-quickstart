35878d605ae66de0638f5d6dfddb8d2f259f3ecf
Costin Costache <cocostac@adobe.com>
2023-02-04 12:36:37 +0200

    Node Example Server - Local Dev Setup

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+import cors from 'cors'
+import express from 'express'
+
+const app = express()
+app.use(cors())
+
+app.get('/', (req, res) => {
+    const testDebug = 4;
+    res.send('Hello World!')
+})
+
+const port = process.env.APP_PORT
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+})
